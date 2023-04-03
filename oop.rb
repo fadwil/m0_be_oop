b748c99921a2a96bf3910adb858ee7a08b490c51
Wil Fady <wrfady@gmail.com>
2023-04-02 21:47:29 -0400

    Add first three classes

--- oop.rb
+++ oop.rb
@@ -6,7 +6,7 @@
 class Unicorn
     def initialize(name)
         @name = name
-        @color = silver
+        @color = "silver"
     end
     def say 
         return "*~* Unicorns DO exist *~*"
@@ -22,6 +22,27 @@ p unicorn1
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
+class Vampire
+    attr_reader :name, :pet, :thirsty
+    def initialize(name, pet = "bat")
+        @name = name
+        @pet = pet
+        @thirsty = true 
+    end
+    def drink
+        @thirsty = false 
+    end
+end
+        
+vampire1 = Vampire.new("Laszlo")
+p vampire1
+
+vampire2 = Vampire.new("Nandor", "owl")
+p vampire2
+
+vampire3 = Vampire.new("Nadja")
+p vampire3.drink
+
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -29,6 +50,27 @@ p unicorn1
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+
+class Dragon
+    attr_reader :name, :rider, :color, :is_hungry
+    def initialize(name, rider, color)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = true
+    end
+    def eat(times)
+        if times == 4
+            is_hungry = false
+        end
+    end
+end
+
+dragon1 = Dragon.new("Yip", "Yop", "Purple")
+p dragon1
+p dragon1.eat(4)
+
+        
 
 
 #  Write a Hobbit class
@@ -39,3 +81,5 @@ p unicorn1
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+
